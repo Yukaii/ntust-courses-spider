cc85a5e51450cec12e2a34e06d212b373f60115c
Yukai Huang <yukaihuang1993@hotmail.com>
2014-12-23 22:09:17 +0800

    general form hashing / add url field

--- course.rb
+++ course.rb
@@ -6,7 +6,7 @@ class Course
                 :course_limitations, :people_in_course, :course_time_location,
                 :english_course_title, :prerequisites, :course_website,
                 :course_objective, :outline, :textbook, :references,
-                :notice, :grading, :note, :about
+                :notice, :grading, :note, :about, :url
   # attr_accessor :attributes
 
   def initialize(h)
@@ -15,39 +15,39 @@ class Course
                    :course_limitations, :people_in_course, :course_time_location,
                    :english_course_title, :prerequisites, :course_website,
                    :course_objective, :outline, :textbook, :references,
-                   :notice, :grading, :note, :about]
+                   :notice, :grading, :note, :about, :url]
     
     h.each {|k,v| send("#{k}=",v)}
   end
   
   def to_hash
-    # @data = Hash[ @attributes.map {|d| [d.to_s, self.instance_variable_get('@'+d.to_s)]} ]
-    {
-      "course_title" => @course_title,
-      "course_code" => @course_code,
-      "lecturer" => @lecturer, 
-      "credits" => @credits,
-      "required_or_elective" => @required_or_elective,
-      "full_or_half_semester" => @full_or_half_semester,
-      "semester" => @semester,
-      "course_limitations" => @course_limitations,
-      "people_in_course" => @people_in_course,
-      "course_time_location" => @course_time_location,
-      "about" => @about,
+    @data = Hash[ @attributes.map {|d| [d.to_s, self.instance_variable_get('@'+d.to_s)]} ]
+    # {
+    #   "course_title" => @course_title,
+    #   "course_code" => @course_code,
+    #   "lecturer" => @lecturer, 
+    #   "credits" => @credits,
+    #   "required_or_elective" => @required_or_elective,
+    #   "full_or_half_semester" => @full_or_half_semester,
+    #   "semester" => @semester,
+    #   "course_limitations" => @course_limitations,
+    #   "people_in_course" => @people_in_course,
+    #   "course_time_location" => @course_time_location,
+    #   "about" => @about,
 
-      "detail" => {
-        "english_course_title" => @english_course_title,
-        "prerequisites" => @prerequisites,
-        "course_website" => @course_website,
-        "course_objective" => @course_objective,    
-        "outline" => @outline,
-        "textbook" => @textbook,
-        "references" => @references,
-        "notice" => @notice,
-        "grading" => @grading,
-        "note" => @note,
-      }
-    }    
+    #   "detail" => {
+    #     "english_course_title" => @english_course_title,
+    #     "prerequisites" => @prerequisites,
+    #     "course_website" => @course_website,
+    #     "course_objective" => @course_objective,    
+    #     "outline" => @outline,
+    #     "textbook" => @textbook,
+    #     "references" => @references,
+    #     "notice" => @notice,
+    #     "grading" => @grading,
+    #     "note" => @note,
+    #   }
+    # }    
   end
 
   def to_json
